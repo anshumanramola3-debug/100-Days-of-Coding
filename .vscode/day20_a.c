b37f0158577cef5c0a1de650cbec88c2e691a887
anshumanramola3-debug <anshumanramola3@gmail.com>
2026-08-30 17:28:15 +0530

    day 20 code a

---
+++ .vscode/day20_a.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, product = 1, digit, hasOdd = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while (n != 0) {
+        digit = n % 10;
+        if (digit % 2 != 0) {
+            product *= digit;
+            hasOdd = 1;
+        }
+        n /= 10;
+    }
+    if (hasOdd) {
+        printf("Product of odd digits is %d", product);
+    } else {
+        printf("No odd digits found.");
+    }
+    return 0;
+}

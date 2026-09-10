d459d986a864aa7bd03b1ad2681b60e6c5cdad9c
anshumanramola3-debug <anshumanramola3@gmail.com>
2026-09-11 03:06:49 +0530

    Day 29 code b

---
+++ .vscode/day29_b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int n,i,max,min;
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter %d elements:\n",n);
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+    max=min=a[0];
+    for(i=1;i<n;i++){
+        if(a[i]>max) max=a[i];
+        if(a[i]<min) min=a[i];
+    }
+    printf("Maximum = %d\nMinimum = %d\n",max,min);
+    return 0;
+}
